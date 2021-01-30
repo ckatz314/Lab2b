7aae91f43c7be419eedb8cbd8eac95f7a48fbab9
ckatz <S00550709@acad.tri-c.edu>
2021-01-30 21:56:07 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.Write ("Enter a number: ");
+    int num1=Convert.ToInt32(Console.ReadLine());
+    Console.Write ("Enter another number: ");
+    int num2=Convert.ToInt32(Console.ReadLine());
+    int sum=num1+num2;
+    Console.WriteLine("Sum = "+sum);
+    int diff=num1-num2;
+    Console.WriteLine("Difference = "+diff);
+    int product=num1*num2;
+    Console.WriteLine("Product = "+product);
+    int quotient=num1/num2;
+    Console.WriteLine("Quotient = "+quotient);
+    int remainder=num1%num2;
+    Console.WriteLine("Remainder = "+remainder);
+
+
+  }
+}
